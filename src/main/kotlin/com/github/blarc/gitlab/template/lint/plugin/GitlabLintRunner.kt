89327b1919fbf30433e718840a7dbe78a42d109c
Blarc <jakob.malezic@gmail.com>
2022-05-17 21:51:09 +0200

    [feat] Add example of running command line program.

--- src/main/kotlin/com/github/blarc/gitlab/template/lint/plugin/GitlabLintRunner.kt
+++ src/main/kotlin/com/github/blarc/gitlab/template/lint/plugin/GitlabLintRunner.kt
@@ -1,5 +1,7 @@
 package com.github.blarc.gitlab.template.lint.plugin
 
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.util.ExecUtil
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import kotlinx.serialization.decodeFromString
@@ -8,11 +10,13 @@ import okhttp3.FormBody
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.yaml.snakeyaml.Yaml
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 import java.time.Duration
 
 
 @Service
-class GitlabLintRunner(project: Project) {
+class GitlabLintRunner(private val project: Project) {
     companion object {
         private val timeout = Duration.ofSeconds(30)
         private val json = Json { ignoreUnknownKeys = true }
@@ -58,7 +62,22 @@ class GitlabLintRunner(project: Project) {
         catch (e: Exception) {
             e.printStackTrace()
         }
+
+        val generalCommandLine = GeneralCommandLine(arrayListOf("git", "status"))
+        generalCommandLine.setCharset(Charset.forName("UTF-8"))
+        generalCommandLine.setWorkDirectory(project.basePath)
+
+        val execAndGetOutput = ExecUtil.execAndGetOutput(generalCommandLine)
+        println(execAndGetOutput)
+
+        "https://gitlab.medius.si/api/v4/projects?search=jakob-test"
+
         return listOf()
+    }
+
+    private fun createCommand(): GeneralCommandLine {
+        return GeneralCommandLine()
+            .withCharset(StandardCharsets.UTF_8)
     }
 
 
