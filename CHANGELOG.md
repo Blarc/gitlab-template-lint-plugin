e2ba1c9ab38f587c430a9ddf1275656dc76d15d1
Blarc <jakob.malezic@gmail.com>
2022-06-22 18:55:58 +0200

    [ci] Add basic GitHub actions workflow.

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Changelog
+
+## [Unreleased]
+### Added
+- Added a basic table with remotes and project ids
+- Added GitHub actions workflows
+
+## [0.0.3]
+### Added
+- Added basic documentation for installation with zip
+- Added OkHttp dependency
+- Added Kotlin plugin dependency
+
+## [0.0.2]
+### Added
+- Use the current branch for linting
+- Search for project by project name and project url
+
+### Changed
+- Removed checking for valid yaml, because Gitlab CI template is not necessarily a valid yaml
+
+## [0.0.1]
+### Added
+- Setting private Gitlab token
+- Error message on linting error
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key).toString()
@@ -6,6 +7,9 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.20"
     id("org.jetbrains.intellij") version "1.5.2"
     kotlin("plugin.serialization") version "1.4.21"
+
+    // Gradle Changelog Plugin
+    id("org.jetbrains.changelog") version "1.3.1"
 }
 
 group = properties("pluginGroup")
@@ -26,6 +30,11 @@ intellij {
         .filter(String::isNotEmpty))
 }
 
+changelog {
+    version.set(properties("pluginVersion"))
+    groups.set(emptyList())
+}
+
 tasks {
     // Set the JVM compatibility versions
     properties("javaVersion").let {
@@ -43,8 +52,29 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("212")
-        untilBuild.set("222.*")
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        pluginDescription.set(
+            projectDir.resolve("README.md").readText().lines().run {
+                val start = "<!-- Plugin description -->"
+                val end = "<!-- Plugin description end -->"
+
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end))
+            }.joinToString("\n").run { markdownToHTML(this) }
+        )
+
+        // Get the latest available change notes from the changelog file
+        changeNotes.set(provider {
+            changelog.run {
+                getOrNull(properties("pluginVersion")) ?: getLatest()
+            }.toHTML()
+        })
     }
 
     signPlugin {
@@ -54,7 +84,9 @@ tasks {
     }
 
     publishPlugin {
+        dependsOn("patchChangelog")
         token.set(System.getenv("PUBLISH_TOKEN"))
+        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
     }
 }
 
