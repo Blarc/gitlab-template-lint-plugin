6f2b4746438d41fd3a2c854d051e0bd62e5e6a3a
Blarc <jakob.malezic@gmail.com>
2023-01-15 12:34:07 +0100

    [chore] Bump dependencies versions.
    org.jetbrains.kotlin.jvm: 1.7.22 -> 1.8.0
    org.jetbrains.intellij: 1.11.0 -> 1.12.0
    plugin.serialization: 1.7.22 -> 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.7.22"
-    id("org.jetbrains.intellij") version "1.11.0"
-    kotlin("plugin.serialization") version "1.7.22"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
+    id("org.jetbrains.intellij") version "1.12.0"
+    kotlin("plugin.serialization") version "1.8.0"
 
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.0.0"
