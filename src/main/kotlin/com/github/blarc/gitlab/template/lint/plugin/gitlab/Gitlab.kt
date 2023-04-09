d7748f1cf124a61e59aa142976ae20e46e37d13f
Blarc <jakob.malezic@gmail.com>
2023-04-09 11:57:22 +0200

    [feat] Refactor Gitlab API client.

--- src/main/kotlin/com/github/blarc/gitlab/template/lint/plugin/gitlab/Gitlab.kt
+++ src/main/kotlin/com/github/blarc/gitlab/template/lint/plugin/gitlab/Gitlab.kt
@@ -37,34 +37,14 @@ open class Gitlab(val project: Project) {
     fun getVersion(
         baseUrl: String,
         gitlabToken: String
-    ): CompletableFuture<GitlabVersion> {
-        val result: CompletableFuture<GitlabVersion> = CompletableFuture<GitlabVersion>()
-
+    ): CompletableFuture<GitlabVersion?> {
         val request: Request = prepareRequest(baseUrl, gitlabToken, "/version")
             .get()
             .build()
 
-        httpClient.newCall(request)
-            .enqueue(object: Callback {
-                override fun onFailure(call: Call, e: IOException) {
-                    result.completeExceptionally(e)
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    response.use {
-                        if (it.isSuccessful) {
-                            val responseString = it.body!!.string()
-                            val decodeFromString = json.decodeFromString<GitlabVersion>(responseString)
-                            result.complete(decodeFromString)
-                        }
-                        else {
-                            result.completeExceptionally(Throwable(it.message.ifEmpty { "Request was unsuccessful!" }))
-                        }
-                    }
-                }
-
-            })
-        return result
+        return makeRequest(request, false) {
+            json.decodeFromString<GitlabVersion>(it)
+        }
     }
 
     fun searchProjectId(
@@ -72,56 +52,24 @@ open class Gitlab(val project: Project) {
         gitlabToken: String,
         remoteUrl: String
     ): CompletableFuture<Long?> {
-
         val projectName = remoteUrl.split("/").last()
-        val request: Request = prepareRequest(baseUrl, gitlabToken,"/projects?search=$projectName")
+        val request: Request = prepareRequest(baseUrl, gitlabToken, "/projects?search=$projectName")
             .get()
             .build()
 
-        val result = CompletableFuture<Long?>()
-        httpClient.newCall(request)
-            .enqueue(object: Callback{
-                override fun onFailure(call: Call, e: IOException) {
-                    sendNotification(Notification.remoteIdNotFound(project), project)
-                    result.complete(null)
-                }
+        return makeRequest(request, false) {
+            val gitlabProjects = json.decodeFromString<Array<GitlabProject>>(it)
+            val gitlabProject = gitlabProjects.find { gitlabProject -> gitlabProject.webUrl.equals(remoteUrl, true) }
+            val gitlabProjectId = gitlabProject?.id
 
-                override fun onResponse(call: Call, response: Response) {
-                    response.use {
-                        if (it.isSuccessful) {
-                            val responseString = it.body!!.string()
-                            val gitlabProjects = json.decodeFromString<Array<GitlabProject>>(responseString)
-                            val gitlabProject = gitlabProjects.find { gitlabProject -> gitlabProject.webUrl.equals(remoteUrl, true) }
-                            val gitlabProjectId = gitlabProject?.id
-
-                            if (gitlabProjectId == null) {
-                                sendNotification(Notification.remoteIdNotFound(project), project)
-                            }
-
-                            result.complete(gitlabProjectId)
-
-                        }
-                        else {
-                            when(it.code) {
-                                401 -> {
-                                    sendNotification(Notification.unauthorizedRequest(project), project)
-                                    result.complete(null)
-                                }
-                                else -> {
-                                    sendNotification(Notification.remoteIdNotFound(project), project)
-                                    result.complete(null)
-                                }
-                            }
-                        }
-                    }
-                }
-
-            })
-
-        return result
+            if (gitlabProjectId == null) {
+                sendNotification(Notification.remoteIdNotFound(project), project)
+            }
+            gitlabProjectId
+        }
     }
 
-    fun lintContent(
+    fun lintTemplate(
         baseUrl: String,
         gitlabToken: String,
         content: String,
@@ -136,14 +84,14 @@ open class Gitlab(val project: Project) {
             .add("dry_run", "true")
             .build()
 
-        val request = prepareRequest(baseUrl, gitlabToken,"/projects/${remoteId}/ci/lint")
+        val request = prepareRequest(baseUrl, gitlabToken, "/projects/${remoteId}/ci/lint")
             .header("Content-Type", "application/json")
             .post(formBody)
             .build()
 
         val result = CompletableFuture<GitlabLint?>()
         httpClient.newCall(request)
-            .enqueue(object: Callback{
+            .enqueue(object : Callback {
                 override fun onFailure(call: Call, e: IOException) {
                     if (showGitlabTokenNotification) {
                         sendNotification(Notification.unsuccessfulRequest(e.localizedMessage))
@@ -155,34 +103,40 @@ open class Gitlab(val project: Project) {
                     response.use {
                         if (it.isSuccessful) {
                             val responseString = it.body!!.string()
-                            val decodeFromString = json.decodeFromString<GitlabLint>(responseString)
-                            result.complete(decodeFromString)
-                        }
-                        else {
+                            result.complete(deserialize(responseString))
+                        } else {
                             if (it.code == 401) {
                                 if (showGitlabTokenNotification) {
                                     sendNotification(Notification.unauthorizedRequest(project), project)
                                 }
                                 result.complete(null)
-                            }
-                            else if (it.code >= 500) {
+                            } else if (it.code >= 500) {
                                 if (showGitlabTokenNotification) {
-                                    sendNotification(Notification.unsuccessfulRequest(message("notifications.internal-server-error", it.code)))
+                                    sendNotification(
+                                        Notification.unsuccessfulRequest(
+                                            message(
+                                                "notifications.internal-server-error",
+                                                it.code
+                                            )
+                                        )
+                                    )
                                 }
                                 result.complete(null)
-                            }
-                            else {
+                            } else {
                                 if (showGitlabTokenNotification) {
-                                    sendNotification(Notification.unsuccessfulRequest(it.message.ifEmpty { message("notifications.unknown-error", it.code) }))
+                                    sendNotification(Notification.unsuccessfulRequest(it.message.ifEmpty {
+                                        message(
+                                            "notifications.unknown-error",
+                                            it.code
+                                        )
+                                    }))
                                 }
                                 result.complete(null)
                             }
                         }
                     }
                 }
-
             })
-
         return result
     }
 }
